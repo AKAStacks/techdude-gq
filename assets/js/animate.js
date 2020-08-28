949a5832a56c26dc6da28d6b4683e401288e6cf9
Dennis Z <denniszarger@gmail.com>
2020-08-28 17:38:57 -0400

    OK yeah.

--- assets/js/animate.js
+++ assets/js/animate.js
@@ -1,11 +1,21 @@
 (function() {
-    var animInterval;
-
-    theSVG = document.getElementById("pcbtraces").getSVGDocument();
-    twoStop = theSVG.getElementById("stop1055");
-    threeStop = theSVG.getElementById("stop1061");
+    animInterval = 0;
 
     let pausedFrames = 0;
+
+    function init() {
+        theSVG = document.getElementById("pcbtraces").getSVGDocument();
+
+        if (theSVG == null) {
+            return;
+        } else {
+            twoStop = theSVG.getElementById("stop1055");
+            threeStop = theSVG.getElementById("stop1061");
+
+            startAnim();
+            document.getElementById("logo").addEventListener("mouseover",startAnim);
+        }
+    }
 
     function reset() {
         pausedFrames = 0;
@@ -15,20 +25,16 @@
     }
 
     function frame() {
-        let twoStopOff = parseFloat(twoStop.getAttribute("offset"));
         let threeStopOff = parseFloat(threeStop.getAttribute("offset"));
+        let twoStopOff = parseFloat(twoStop.getAttribute("offset"));
 
         if (threeStopOff < 1) {
             threeStop.setAttribute("offset", threeStopOff + 0.05);
-            return;
-        /* } else if (pausedFrames < 1) {
-            pausedFrames++;
-            return; */
         } else if (twoStopOff < 1) {
-            twoStop.setAttribute("offset", twoStopOff + 0.025);
-            return;
+            twoStop.setAttribute("offset", twoStopOff + 0.05);
         } else {
             clearInterval(animInterval);
+            animInterval = 0;
         }
     }
 
@@ -36,16 +42,21 @@
         let logo = document.getElementById("logo");
         let pcbElement = document.getElementById("pcbtraces");
 
-        let ydiff = ((logo.clientHeight/2) - (pcbElement.clientHeight/2))/2;
-        pcbElement.style.top = Math.round(ydiff) + "px";
+        console.log(pcbElement.clientHeight);
+        console.log(window.getComputedStyle(pcbElement).getPropertyValue('height'));
+
+        let ydiff = ((pcbElement.clientHeight/2) - (logo.getBoundingClientRect().bottom - (logo.clientHeight/2)))
+        pcbElement.style.top = Math.round(-1*ydiff) + "px";
     }
 
     function startAnim() {
+        center();
         reset();
+        if (animInterval != 0) {
+            clearInterval(animInterval);
+        }
         animInterval = setInterval(frame, 60);
     }
 
-    center();
-    startAnim();
-    document.getElementById("logo").addEventListener("mouseover",startAnim);
+    animInterval = setTimeout(init, 60);
 })();
